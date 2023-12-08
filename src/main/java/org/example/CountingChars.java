92bc767d4b1c63166c8b356354008727ff8c24d1
Doyle Brown <dalpiad@gmail.com>
2023-12-07 20:17:26 -0600

    Completed class 3 studio.

--- src/main/java/org/example/CountingChars.java
+++ src/main/java/org/example/CountingChars.java
@@ -1,34 +1,39 @@
 package org.example;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.HashMap;
 
 public class CountingChars {
     public static void main(String[] args) {
+        String quoteFromFile = "";
+
+        try {
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occured while trying to access the file.");
+        }
+
         Scanner input = new Scanner(System.in);
 
-        String statement = "If the product of two terms is zero then common " +
-                "sense says at least one of the two terms has to be zero to " +
-                "start with. So if you move all the terms over to one side, " +
-                "you can put the quadratics into a form that can be factored" +
-                " allowing that side of the equation to equal zero. Once you’ve " +
-                "done that, it’s pretty straightforward from there.";
-
         System.out.println("Enter a word, sentence or paragraph to evaluate. ");
-        statement = input.nextLine();
+        String statement = input.nextLine();
 
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        char[] charactersInString = statement.toLowerCase().toCharArray();
-
+        char[] charactersInString = statement.toUpperCase().toCharArray();
 
         HashMap<Character, Integer> charsCount = new HashMap<>();
 
         for (Character letter : charactersInString) {
-            if (charsCount.containsKey(letter)) {
-                charsCount.put(letter, (charsCount.get(letter) +1));
-            } else {
-                charsCount.put(letter, 1);
+            if (alphabet.indexOf(letter) >= 0) {
+                if (charsCount.containsKey(letter)) {
+                    charsCount.put(letter, (charsCount.get(letter) +1));
+                } else {
+                    charsCount.put(letter, 1);
+                }
             }
+
         }
         for (Character i : charsCount.keySet()) {
             System.out.println(i + ": " + charsCount.get(i));
